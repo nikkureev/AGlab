897bcb47884cd54adc90ff616adabda66018ba5c
nikkureev <32942796+nikkureev@users.noreply.github.com>
2020-11-08 14:18:09 +0300

    Update FASTA.py

--- FASTA.py
+++ FASTA.py
@@ -15,3 +15,30 @@ def fastaParser(infile):
                 sequence += line.rstrip()
         seqs.append(sequence)
     return headers, seqs
+
+
+def FASTA(filename):
+    try:
+        f = open(filename,"r")
+    except IOError:
+        print ('The file, {}, does not exist'.format(filename))
+        return
+
+    order = []
+    sequences = {}
+
+    for line in f:
+        if line.startswith('>'):
+            name = line[1:].rstrip('\n')
+            name = name.replace('_',' ')
+            order.append(name)
+            sequences[name] = ''
+
+        else:
+            sequences[name] += line.rstrip('\n').rstrip('*')
+
+    print ('{} sequences found'.format(len(order)))
+    return order, sequences
+
+
+FASTA('C:/AGlab/one.txt')
